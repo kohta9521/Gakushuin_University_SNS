768d53de40682bb3fb62cb78491e6ea062e53493
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-05 12:41:54 +0900

    add routing

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const PORT = 3000;
 // const userRoute = require("./routes/users");
 
-// ミドルウェア
-// app.use("/api/users", userRoute);
-
 app.get("/", (req, res) => {
     res.send("Hello express");
+})
+
+app.get("/users", (req, res) => {
+    res.send("users express");
 })
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
