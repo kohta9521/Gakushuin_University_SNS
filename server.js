58ded63d976c3ffe958e70647484957ac4371997
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-08 12:56:36 +0900

    add sever.js

--- server.js
+++ server.js
@@ -2,4 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello Express!!!");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Users Express!!!");
+});
+
 app.listen(PORT, () => console.log("サーバーが起動しました"));
