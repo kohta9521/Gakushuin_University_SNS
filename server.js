0da5486dcdabe22aa90a292c2d83f043aa00555b
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-08 13:05:40 +0900

    user routes setting

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
 const PORT = 3000;
+
+// ミドルウェアの設定
+app.use("/api/users", userRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello Express!!!");
 });
 
-app.get("/users", (req, res) => {
-  res.send("Users Express!!!");
-});
+// app.get("/users", (req, res) => {
+//   res.send("Users Express!!!");
+// });
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
