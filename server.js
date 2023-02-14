6a69e2ce8c53fab33468df8333fba8f43a351638
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-14 14:27:03 +0900

    add res and req localhost

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("node");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+app.listen(PORT, () => console.log("サーバーが起動しました"));
