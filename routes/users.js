855448811f55b80660c4610b5de761b4d04f812f
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-14 21:43:37 +0900

    add ユーザーフォローのAPIを自作

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { request } = require("express");
 const User = require("../models/user");
 
 // CRUD操作
@@ -47,8 +48,36 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// router.get("/", (req, res) => {
-//   res.send("user router");
-// });
+// ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        // フォロワーに自分がいなかったらフォローできる
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+        return request.statusCode(200).json("フォローに成功しました!");
+      } else {
+        return res
+          .status(403)
+          .json("あなたはすでにこのユーザーをフォローしています");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json("自分自身をフォローできません");
+  }
+});
 
 module.exports = router;
