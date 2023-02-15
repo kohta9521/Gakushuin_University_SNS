eeb3117975e301a0b4df39939e14358c9d12144b
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-15 18:22:20 +0900

    投稿スキーマの作成

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+const mogoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 200,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mogoose.models("Post", PostSchema);
